3c6c50fa6b2237a07d5116ce5102ca7446de2280
LC-daoxin <457121827@qq.com>
2019-11-15 20:39:31 +0800

    add

---
+++ src/components/Tabbar/Tabbottom.vue
@@ -0,0 +1,57 @@
+<template>
+  <van-tabbar
+    class="tabbar"
+    v-model="active"
+    active-color="#2196F3"
+    safe-area-inset-bottom
+  >
+    <van-tabbar-item icon="home-o" to="/Home" replace>主页</van-tabbar-item>
+    <van-tabbar-item icon="todo-list-o" info="5" to="/List" replace>待办</van-tabbar-item>
+    <van-tabbar-item>
+      <div class="icon-middle" slot="icon">
+        <i class="iconfont iconAdd">&#xe61d;</i>
+      </div>
+    </van-tabbar-item>
+    <van-tabbar-item icon="search">搜索</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vant'
+export default {
+  name: 'TabBottom',
+  props: {
+    CurrentPage: Number
+  },
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  },
+  data () {
+    return {
+      active: this.CurrentPage
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@styles/main.scss';
+  .tabbar{
+    font-weight: 400;
+    >>> .van-tabbar-item{
+      color: #555;
+      .van-icon{
+        font-weight: 500;
+      }
+    }
+    .icon-middle{
+      padding-top: .08rem;
+      .iconAdd{
+        font-size: .8rem;
+        color: $bgColor;
+      }
+    }
+  }
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="list">
+    <form action="/">
+      <van-search
+        v-model="value"
+        placeholder="请输入搜索关键词"
+        show-action
+      >
+        <div slot="action">搜索</div>
+      </van-search>
+    </form>
+    <van-tabs
+      v-model="tabactive"
+      :ellipsis="ellipsis"
+      color="#2196F3"
+      title-active-color="#2196F3"
+      swipeable
+    >
+      <van-tab>
+        <div slot="title" class="tab">
+          <i class="iconfont">&#xeb93;</i> My Pending
+        </div>
+        <div></div>
+      </van-tab>
+      <van-tab>
+        <div slot="title" class="tab">
+          <i class="iconfont">&#xeb9a;</i> BM Pending
+        </div>
+        <div></div>
+      </van-tab>
+    </van-tabs>
+    <tab-bottom :CurrentPage="CurrentPage"></tab-bottom>
+  </div>
+</template>
+
+<script>
+import TabBottom from '@/components/Tabbar/Tabbottom'
+import { Search, Tab, Tabs } from 'vant'
+export default {
+  name: 'list',
+  components: {
+    [Search.name]: Search,
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
+    TabBottom
+  },
+  data () {
+    return {
+      value: '',
+      CurrentPage: 1,
+      tabactive: 0,
+      ellipsis: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .list{
+    width: 100%;
+    height: 100vh;
+    background-color: #f1f1f1;
+    position: relative;
+    .tab{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      i{
+        margin-right: 5px;
+      }
+    }
+  }
+</style>
