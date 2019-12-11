2b04a7b5d2cabc223ab0d88ecfc8414de5cf71d3
LC-daoxin <457121827@qq.com>
2019-12-11 11:30:21 +0800

    update

--- src/global/axios.js
+++ src/global/axios.js
@@ -1,4 +1,7 @@
+import Vue from 'vue'
 import axios from 'axios'
+import { Toast } from 'vant'
+Vue.use(Toast)
 // create an axios instance
 const service = axios.create({
   baseURL: '',
@@ -6,8 +9,13 @@ const service = axios.create({
   timeout: 300000
 })
 const localStorage = window.localStorage
-// request interceptor
+// request interceptor 请求处理
 service.interceptors.request.use(config => {
+  Toast.loading({
+    message: '加载中...',
+    forbidClick: true,
+    duration: 0
+  })
   const paiToken = localStorage.getItem('PAI-token')
   console.log(config)
   if (paiToken) {
@@ -15,8 +23,20 @@ service.interceptors.request.use(config => {
   return config
 }, error => {
   // Do something with request error
-  console.log(error)
-  Promise.reject(error)
+  // 请求错误 状态码 401超时 404 not found
+  console.log('request' + error)
+  return Promise.reject(error)
 })
-
+// 响应处理
+service.interceptors.response.use(res => {
+  console.log(res)
+  Toast.clear()
+  return res
+}, error => {
+  // 响应错误处理
+  // http 状态码 500 系统错误 502 系统重启
+  console.log('response' + error)
+  Toast.clear()
+  return Promise.reject(error)
+})
 export default service
